8f376d9675d3e78d26dacc0cf9d4874f810213d8
javaorc <1241061322@qq.com>
2019-07-03 23:56:13 +0800

    解决冲突

---
+++ src/main/java/com/cskaoyan/controller/MallRegionController.java
@@ -0,0 +1,32 @@
+package com.cskaoyan.controller;
+
+import com.cskaoyan.Service.MallRegionService;
+import com.cskaoyan.bean.Region;
+import com.cskaoyan.responseVo.ResponseVO1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("admin")
+public class MallRegionController {
+    @Autowired
+    MallRegionService mallRegionService;
+
+    @RequestMapping("region/list")
+    @ResponseBody
+    public ResponseVO1<List<Region>> showArea(){
+
+        List<Region> regions = mallRegionService.showArea();
+        ResponseVO1<List<Region>> responseVO1 = new ResponseVO1<>();
+        responseVO1.setData(regions);
+        responseVO1.setErrmsg("成功");
+        responseVO1.setErrno(0);
+        System.out.println(regions);
+        return responseVO1;
+    }
+}
