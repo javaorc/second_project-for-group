ba68e06ed50005e9304e37ab7e5c78b768964cc2
xs962617107 <45117570+xs962617107@users.noreply.github.com>
2019-07-04 22:57:00 +0800

    优惠券管理模块基本完成

--- src/main/java/com/cskaoyan/mapper/generalize/CouponMapper.java
+++ src/main/java/com/cskaoyan/mapper/generalize/CouponMapper.java
@@ -1,8 +1,10 @@
 package com.cskaoyan.mapper.generalize;
 
 import com.cskaoyan.bean.Coupon;
+import com.cskaoyan.bean.CouponUser;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,4 +17,18 @@ public interface CouponMapper {
     List<Coupon> findCouponList(@Param("name") String name,
                                 @Param("type")Short type,
                                 @Param("status")Short status);
+
+    int insertCoupon(@Param("coupon") Coupon coupon);
+
+    Coupon readCouponById(@Param("id") int id);
+
+    List<CouponUser> queryUserByCouponId(@Param("id") int couponId,
+                                         @Param("userId") Integer userId,
+                                         @Param("status") Integer status);
+
+    Coupon queryCouponByAddTime(@Param("date") Date date);
+
+    int updateCoupon(@Param("coupon") Coupon coupon);
+
+    int deleteCoupon(@Param("coupon") Coupon coupon);
 }
