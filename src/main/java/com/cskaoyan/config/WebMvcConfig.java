ac939040d74c533c940bfd99a707efdd952ed648
wubing1178 <837137118@qq.com>
2019-07-05 11:00:57 +0800

    goods update

--- src/main/java/com/cskaoyan/config/WebMvcConfig.java
+++ src/main/java/com/cskaoyan/config/WebMvcConfig.java
@@ -1,16 +1,24 @@
 package com.cskaoyan.config;
 
+import com.cskaoyan.converter.MapToQueryMapVOConverter;
 import com.cskaoyan.converter.StringToDateConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.support.ConfigurableConversionService;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Autowired
-    StringToDateConverter stringToDateConverter;
+
     @Autowired
     ConfigurableConversionService conversionService;
+
+    @Autowired
+    StringToDateConverter stringToDateConverter;
+
+    @Autowired
+    MapToQueryMapVOConverter mapToQueryMapVOConverter;
+
     public void addConverter(){
         conversionService.addConverter(stringToDateConverter);
+        conversionService.addConverter(mapToQueryMapVOConverter);
     }
 }
