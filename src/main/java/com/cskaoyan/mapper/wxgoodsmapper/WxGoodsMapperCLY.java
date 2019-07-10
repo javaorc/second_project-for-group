76070d0157f15bd56de1d56b9a9972fd70ffe105
ChenLianYuan <algorithm.enthusiast@gmail.com>
2019-07-10 16:50:24 +0800

    update

--- src/main/java/com/cskaoyan/mapper/wxgoodsmapper/WxGoodsMapperCLY.java
+++ src/main/java/com/cskaoyan/mapper/wxgoodsmapper/WxGoodsMapperCLY.java
@@ -1,6 +1,7 @@
 package com.cskaoyan.mapper.wxgoodsmapper;
 
 import com.cskaoyan.bean.*;
+import com.cskaoyan.bean.wxBean.WxComment3;
 import com.cskaoyan.bean.wxBean.WxGoods2;
 import org.apache.ibatis.annotations.Param;
 
@@ -24,8 +25,10 @@ public interface WxGoodsMapperCLY {
 
     Goods queryInfo(@Param("id") String id);
 
-    List<Comment> queryCommentDate(@Param("id") String id);
+    List<WxComment3> queryCommentDate(@Param("id") String id);
 
     Integer queryCommentCount(@Param("id") String id);
+
+    List<Groupon> queryGroupon(String id);
 
 }
--- src/main/java/com/cskaoyan/service/wxcommentservice/WxCommentService.java
+++ src/main/java/com/cskaoyan/service/wxcommentservice/WxCommentService.java
@@ -1,5 +1,15 @@
 package com.cskaoyan.service.wxcommentservice;
 
+import com.cskaoyan.bean.vo.QueryCommentList;
+import com.cskaoyan.bean.wxBean.WxComment2;
+import com.cskaoyan.bean.wxBean.WxCount;
+
+import java.util.List;
+
 public interface WxCommentService {
+
+    QueryCommentList<List<WxComment2>> queryList(String valueId, int type, int size,int page, int showType);
+
+    WxCount getCount();
 
 }
